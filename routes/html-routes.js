1f7e15ea8be7b8aa24563de69444537f9d9d2db4
Mariela Velez <Mariela@marielas-air.attlocal.net>
2017-10-09 05:07:37 -0500

    Beginning my portfolio. Will eventually deploy to Heroku.

---
+++ routes/html-routes.js
@@ -0,0 +1,14 @@
+// Dependencies
+// =============================================================
+var path = require("path");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+  
+  // index route loads view.html
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+
+};
