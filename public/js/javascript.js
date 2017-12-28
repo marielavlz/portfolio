c5f40f362e35d96e571b5f9e1a147a7aee8c4494
Mariela Velez <Mariela@marielas-air.attlocal.net>
2017-12-28 16:41:20 -0600

    Changed my entire portfolio. No longer built on a template. Just my own. Still not finished. Missing Contact page and changing the work section.

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -1,44 +1,229 @@
 $(document).ready(function() {
 
-	//ACCORDION BUTTON ACTION (ON CLICK DO THE FOLLOWING)
-	$('.accordionButton').click(function() {
+// toggle class scroll
+$(window).scroll(function() {
+    if($(this).scrollTop() > 50)
+    {
+        $('.navbar-trans').addClass('afterscroll');
+    } else
+    {
+        $('.navbar-trans').removeClass('afterscroll');
+    }
 
-		//REMOVE THE ON CLASS FROM ALL BUTTONS
-		$('.accordionButton').removeClass('on');
+});
 
-		//NO MATTER WHAT WE CLOSE ALL OPEN SLIDES
-	 	$('.accordionContent').slideUp('normal');
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      window.location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      window.location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
 
-		//IF THE NEXT SLIDE WASN'T OPEN THEN OPEN IT
-		if($(this).next().is(':hidden') == true) {
+  //Extra animations
+  // typing animation
+  (function($) {
+    $.fn.writeText = function(content) {
+        var contentArray = content.split(""),
+            current = 0,
+            elem = this;
+        setInterval(function() {
+            if(current < contentArray.length) {
+                elem.text(elem.text() + contentArray[current++]);
+            }
+        }, 80);
+    };
 
-			//ADD THE ON CLASS TO THE BUTTON
-			$(this).addClass('on');
+  })(jQuery);
 
-			//OPEN THE SLIDE
-			$(this).next().slideDown('normal');
-		 }
+  // input text for typing animation
+  $("#holder").writeText("FULL STACK WEB DEVELOPER + MICROBIOLOGIST");
 
-	 });
+  // initialize wow.js
+  new WOW().init();
+
+  // Push the body and the nav over by 285px over
+  var main = function() {
+    $('.fa-bars').click(function() {
+      $('.nav-screen').animate({
+        right: "0px"
+      }, 200);
+
+      $('body').animate({
+        right: "285px"
+      }, 200);
+    });
+
+    // Then push them back */
+    $('.fa-times').click(function() {
+      $('.nav-screen').animate({
+        right: "-285px"
+      }, 200);
+
+      $('body').animate({
+        right: "0px"
+      }, 200);
+    });
+
+    $('.nav-links a').click(function() {
+      $('.nav-screen').animate({
+        right: "-285px"
+      }, 500);
+
+      $('body').animate({
+        right: "0px"
+      }, 500);
+    });
+  };
+
+  $(document).ready(main);
+
+  // initiate full page scroll
+
+  $('#fullpage').fullpage({
+    scrollBar: true,
+    responsiveWidth: 400,
+    navigation: true,
+    navigationTooltips: ['home', 'about', 'portfolio', 'contact', 'connect'],
+    anchors: ['home', 'about', 'portfolio', 'contact', 'connect'],
+    menu: '#myMenu',
+    fitToSection: false,
+
+    afterLoad: function ( anchorLink, index){
+      var loadedSection = $(this);
 
 
-	/*** REMOVE IF MOUSEOVER IS NOT REQUIRED ***/
+      //using index
+      if(index==1){
+        /* add opacity to arrow */
+        $('.fa-chevron-down').each(function(){
+          $(this).css('opacity','1')
+        });
+        $('.header-links a').each(function(){
+          $(this).css('color','white')
+        });
+        $('.header-links').css("background-color","transparent");
+      }
 
-	//ADDS THE .OVER CLASS FROM THE STYLESHEET ON MOUSEOVER
-	$('.accordionButton').mouseover(function() {
-		$(this).addClass('over');
+      else if(index!=1){
+        $('.header-links a').each(function(){
+          $(this).css('color','black')
+        });
+        $('.header-links').css('background-color', 'white');
+      }
 
-	//ON MOUSEOUT REMOVE THE OVER CLASS
-	}).mouseout(function() {
-		$(this).removeClass('over');
-	});
+      //using index
+      if(index == 2){
 
-	/*** END REMOVE IF MOUSEOVER IS NOT REQUIRED ***/
+        /* animate skill bars */
+        $('.skillbar').each(function(){
+          $(this).find('.skillbar-bar').animate({
+            width:jQuery(this).attr('data-percent')
+          },2500);
+        });
+      }
+    }
+  });
 
 
-	/********************************************************************************************************************
-	CLOSES ALL S ON PAGE LOAD
-	********************************************************************************************************************/
-	$('.accordionContent').hide();
+  // move section down one
+  $(document).on('click', '#moveDown', function(){
+    $.fn.fullpage.moveSectionDown();
+  });
+
+  // fullpage.js link navigation
+  $(document).on('click', '#skills', function(){
+    $.fn.fullpage.moveTo(2);
+  });
+
+  $(document).on('click', '#projects', function(){
+    $.fn.fullpage.moveTo(3);
+  });
+
+  $(document).on('click', '#contact', function(){
+    $.fn.fullpage.moveTo(4);
+  });
+
+  //ajax form
+  $(function() {
+
+    // Get the form.
+    var form = $('#ajax-contact');
+
+    // Get the messages div.
+    var formMessages = $('#form-messages');
+
+    // Set up an event listener for the contact form.
+    $(form).submit(function(e) {
+      // Stop the browser from submitting the form.
+      e.preventDefault();
+
+      // Serialize the form data.
+      var formData = $(form).serialize();
+
+      // Submit the form using AJAX.
+      $.ajax({
+        type: 'POST',
+        url: $(form).attr('action'),
+        data: formData
+      })
+      .done(function(response) {
+        // Make sure that the formMessages div has the 'success' class.
+        $(formMessages).removeClass('error');
+        $(formMessages).addClass('success');
+
+        // Set the message text.
+        $(formMessages).text(response);
+
+        // Clear the form.
+        $('#name').val('');
+        $('#email').val('');
+        $('#message').val('');
+      })
+      .fail(function(data) {
+        // Make sure that the formMessages div has the 'error' class.
+        $(formMessages).removeClass('success');
+        $(formMessages).addClass('error');
+
+        // Set the message text.
+        if (data.responseText !== '') {
+          $(formMessages).text(data.responseText);
+        } else {
+          $(formMessages).text('Oops! An error occured and your message could not be sent.');
+        }
+      });
+
+    });
+
+  });
 
 });
