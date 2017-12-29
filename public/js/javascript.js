9704551f35f2d4f3017f1970fb6be06863a86fc2
Mariela Velez <Mariela@marielas-air.attlocal.net>
2017-12-29 00:57:59 -0600

    Overall design is finished. Just have to add functionality to email and then try to deploy. Having issues with heroku at the moment. Also have to replace text with icons in the project cards.

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -12,6 +12,69 @@ $(window).scroll(function() {
 
 });
 
+
+$(".envelope").one("click", function(){
+
+   $(".top-wrap").animate({ textIndent: 180}, {
+    duration: 500,
+    step: function(now) {$(this).css('-webkit-transform',"rotateX(" + now + "deg)");},
+    complete: function(){ $(".top-wrap").animate({zIndex:-1}, 100);}
+  });
+
+  $(".letter-wrap").delay(500).animate({
+    height:'520px',
+    top:'-500px'
+  },500);
+
+  $(".letter-wrap").delay(1000).animate({
+    top:'-200px',
+    zIndex:999
+  },500);
+
+ $(".envelope").delay(500).animate({
+    top:'50px'
+  },500);
+
+});
+
+$(".send").one("click", function(e){
+  e.preventDefault();
+
+   $(".letter-wrap").animate({
+    top:'-500px',
+  },500);
+
+  $(".front").delay(800).animate({
+    zIndex:999
+  },500);
+
+  $(".letter-wrap").delay(1000).animate({
+    top:'0px',
+    height:'200px'
+  },500);
+
+ $(".envelope").delay(1000).animate({
+    top:'0px'
+  },500);
+
+$(".top-wrap").delay(2500).animate({
+    zIndex:999
+  },500);
+
+$(".top-wrap").animate({ textIndent: 0}, {
+    step: function(now,fx) {
+      $(this).css('-webkit-transform',"rotateX(" + now + "deg)");
+    }
+  },500);
+
+  $(".envelope").delay(3000).animate({
+    right:'-5000px'
+  },500).fadeOut(1000, function(){
+      $("p.notif").fadeIn(1000);
+  });
+
+});
+
 // Select all links with hashes
 $('a[href*="#"]')
   // Remove links that don't actually link to anything
